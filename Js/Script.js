bc8fd5f35d05f54a1ba907ce9403904e6a65781a
nathan <https://github.com/blackhaii/Teste-curso-dio.git>
2022-09-26 14:51:20 -0300

    first commit

---
+++ Js/Script.js
@@ -0,0 +1,23 @@
+const mario = document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+
+    if (pipePosition <= 120) {
+
+        pipe.style.animation = 'none';
+        
+    }
+
+}, 10 );
+
+document.addEventListener('keydown', jump);
